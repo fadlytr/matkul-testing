c63b0ac7b0badedb8253e51b49e69f069f0e0488
fadlytr <fadlytriansyah@gmail.com>
2020-02-12 12:55:16 +0700

    masukkan coba1 ke repository

---
+++ coba1/src/coba1/Coba1.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coba1;
+
+/**
+ *
+ * @author SDB_07
+ */
+public class Coba1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
